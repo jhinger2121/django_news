1b2eab37877ea7854bb00a52c735687b134ae33b
kuldeep <kuldeepjhinger212@gmail.com>
2022-06-30 10:51:48 -0400

    make it better

--- django_news_aggregator/settings/local.py
+++ django_news_aggregator/settings/local.py
@@ -11,7 +11,7 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE += [
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Database
@@ -46,3 +46,9 @@ CELERY_TIMEZONE = 'UTC'
 CELERY_IMPORTS = (
     'posts.tasks'
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
